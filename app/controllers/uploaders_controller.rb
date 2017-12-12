daeffdf249e10adb1039e968e025489ea4c6baee
lchirayil <lchirayil@sgcmail.com>
2017-12-12 12:58:24 -0600

    commitment to different branch

--- app/controllers/uploaders_controller.rb
+++ app/controllers/uploaders_controller.rb
@@ -1,5 +1,30 @@
 class UploadersController < ApplicationController
-  def index
+  require 'aws-sdk'
+  def create
+    # Make an object in your bucket for your upload
+    obj = Aws::S3::Resource.new.bucket([ENV['S3_BUCKET']]).objects[params[:file].original_filename]
+
+    # Upload the file
+    obj.write(
+      file: params[:file],
+      acl: :public_read
+    )
+
+    # Create an object for the upload
+    @upload = Upload.new(
+    	url: obj.public_url,
+		  name: obj.key
+    	)
+
+    if @upload.save
+      redirect_to '/'
+    else
+
+      flash.now[:notice] = 'There was an error'
+      redirect_to '/failed'
+    end
+  end
+  def show
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'uploaders#index'
+  get '/failed' => 'uploaders#show'
+  post '/uploaders' => 'uploaders#create'
 end
