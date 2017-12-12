3f4161d8b49f052cb4516a9718c28cff5931e252
lchirayil <lchirayil@sgcmail.com>
2017-12-12 14:15:33 -0600

    pull bucket data

--- app/controllers/uploaders_controller.rb
+++ app/controllers/uploaders_controller.rb
@@ -1,9 +1,13 @@
 class UploadersController < ApplicationController
   require 'aws-sdk'
   def index
+    @s3 = Aws::S3::Client.new
+    @resp = @s3.list_objects(bucket: ENV['S3_BUCKET'])
+
+
     s3 = Aws::S3::Resource.new
     @post = s3.bucket(ENV['S3_BUCKET']).presigned_post(
-      key: "uploads/#{Time.current.year.to_i}/#{Time.current.month.to_i}/${filename}",
+      key: "sgcimages/#{Time.current.year.to_i}/#{Time.current.month.to_i}/${filename}",
       allow_any: ['utf8', 'authenticity_token'],
       acl: "authenticated-read",
     )
