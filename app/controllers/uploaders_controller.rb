eca41080881c2731c3e600bcdf26e40500209746
lchirayil <lchirayil@sgcmail.com>
2017-12-12 16:21:22 -0600

    basic view structure set

--- app/controllers/uploaders_controller.rb
+++ app/controllers/uploaders_controller.rb
@@ -1,12 +1,52 @@
 class UploadersController < ApplicationController
   before_action :load_aws
 
-  def year
-    @objects = @bucket.objects(prefix: "sgcimages/#{params[:year]}")
+  def index
+    @objects = @bucket.objects(prefix: "sgcimages/")
+    url_array = []
+    @resp.contents.each do |object|
+      url_array << object.key
+    end
+    @years = year_strip(url_array)
   end
 
+  def year
+    url_array = []
+    @resp.contents.each do |object|
+      url_array << object.key
+    end
+    @months = month_strip(url_array)
+  end
+
+  def month
+    @objects = @bucket.objects(prefix: "sgcimages/#{params[:year]}/#{params[:month]}")
+  end
 
   private
+
+  def year_strip(url_array)
+    new_array = []
+    years = []
+    url_array.each do |url|
+      new_array << url.split('/')
+      new_array.each do |year|
+        years << year[1]
+      end
+    end
+    years.uniq!
+  end
+
+  def month_strip(url_array)
+    new_array = []
+    months = []
+    url_array.each do |url|
+      new_array << url.split('/')
+      new_array.each do |month|
+        months << month[2]
+      end
+    end
+    months.uniq!
+  end
 
   def load_aws
     require 'aws-sdk'
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'uploaders#index'
   get '/:year' => 'uploaders#year'
-  get '/:month' => 'uploaders#month'
+  get '/:year/:month' => 'uploaders#month'
   post '/uploaders' => 'uploaders#create'
 end
