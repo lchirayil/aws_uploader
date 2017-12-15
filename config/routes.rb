c27005bdd6314764164bb5feb08c217f8f6cceb2
lchirayil <lchirayil@sgcmail.com>
2017-12-15 10:30:24 -0600

    added views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:registrations, :passwords, :confirmations, :mailer]
   root 'uploaders#index'
   get '/:year' => 'uploaders#year'
   get '/:year/:month' => 'uploaders#month'
