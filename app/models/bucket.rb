68f9c641914c965df213a7c08da9fb029b6ac16b
lchirayil <lchirayil@sgcmail.com>
2017-12-19 13:55:51 -0600

    'like' search enabled for wider search results

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -1,4 +1,4 @@
 class Bucket < ApplicationRecord
   include PgSearch
-  pg_search_scope :kinda_spelled_like, :against => :filename
+  pg_search_scope :kinda_spelled_like, :against => :filename, :using => { :tsearch => {:prefix => true, :dictionary => "english"} }
 end
